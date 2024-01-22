a671af1cfa4c25487a75c8d365574f6e4c8cdb65
Christophgru <christoph@familie-gruender.de>
2024-01-22 22:41:57 +0100

    fly to Position, Lots of refactoring

--- fw7empty/src/components/MapComponent.jsx
+++ fw7empty/src/components/MapComponent.jsx
@@ -1,30 +1,44 @@
 // MapComponent.jsx
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const MapComponent = ({ zoom }) => {
   const [userPosition, setUserPosition] = useState([0, 0]);
+  const mapRef = useRef(null);
 
   useEffect(() => {
+    const handleUpdatePosition = (newPosition) => {
+      setUserPosition(newPosition);
+    };
+
+    const handleGeolocationSuccess = (position) => {
+      const { latitude, longitude } = position.coords;
+      const newPosition = [latitude, longitude];
+      console.log(position.coords);
+      handleUpdatePosition(newPosition);
+
+      // Center the map on the user's current position
+      mapRef.current && mapRef.current.setView(newPosition, zoom);
+    };
+
+    const handleGeolocationError = (error) => {
+      console.log(error);
+      // TODO: Implement error handling (e.g., show a message to the user)
+    };
+
     // Use Geolocation API to get the user's current position
     navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setUserPosition([latitude, longitude]);
-      },
-      (error) => {
-        console.log(error)
-        // TODO set position based on ip address
-      }
+        handleGeolocationSuccess,
+        handleGeolocationError
     );
-  }, [])
+  }, [zoom]);
 
   const handleUpdatePosition = (newPosition) => {
     setUserPosition(newPosition);
   };
 
   const customMarkerIcon = new L.Icon({
-    iconUrl: '../markerWithServiceHelper/images/marker-icon.png', 
+    iconUrl: 'icons/marker-icon.png',
     iconSize: [32, 32],
     iconAnchor: [16, 32],
     popupAnchor: [0, -32],
@@ -32,7 +46,7 @@ const MapComponent = ({ zoom }) => {
 
   return (
     <div >
-      <MapContainer center={userPosition} zoom={zoom} style={{ height: '400px', width: '100%' }}>
+      <MapContainer ref={mapRef} center={userPosition} zoom={zoom} style={{ height: '100vh', width: '100%' }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
