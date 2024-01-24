4e248c7a5f6264b86cbbb2fdb366b7501992c2bf
indicatelovelace <mail@neva.blue>
2024-01-24 22:49:22 +0100

    gps permission denied handling

--- SmartMap/src/components/MapComponent.jsx
+++ SmartMap/src/components/MapComponent.jsx
@@ -2,8 +2,14 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
+import { f7, Popup as F7Popup } from 'framework7-react';
+import axios from 'axios';
+import { exact } from 'prop-types';
+
+
 
 const MapComponent = ({ zoom, destinationMarker, initialLocation }) => {
+  const popupRef = useRef(null);
   const [userPosition, setUserPosition] = useState([0, 0]);
   const mapRef = useRef(null);
   const [firstPos, setFirstPos] = useState(0);
@@ -21,23 +27,50 @@ const MapComponent = ({ zoom, destinationMarker, initialLocation }) => {
       handleUpdatePosition(newPosition);
 
       // Center the map on the user's current position
-      if(firstPos===0){
+      if (firstPos === 0) {
         console.log(position.coords);
         mapRef.current && mapRef.current.setView(newPosition, zoom);
         setFirstPos(1);
       }
     };
 
+    const handleGeolocationError = async () => {
+      try {
+        // Fetch user's country using "ipinfo.io"
+        const ipInfoResponse = await axios.get('https://ipinfo.io/json');
+        const { country, loc } = ipInfoResponse.data;
+        // Extract coordinates from the "loc" string
+        const [latitude, longitude] = loc.split(',').map(coord => parseFloat(coord));
+        setUserPosition([latitude, longitude])
+      }
+      catch (error) {
+        popupRef.current = f7.popup.create({
+          content: `
+          <div class="popup"><div class="block">
+          An error occured
+          </div></div>
+            `,
+          on: {
+            closed: () => {
+              // Clean up the popup when it closes
+              popupRef.current = null;
+            },
+          },
+        });
 
-    const handleGeolocationError = (error) => {
-      console.log(error);
-      // TODO: Implement error handling (e.g., show a message to the user)
+        popupRef.current.open();
+
+        // Close the popup after 2 seconds
+        setTimeout(() => {
+          popupRef.current.close();
+        }, 3000)
+      }
     };
 
     // Use Geolocation API to get the user's current position
     navigator.geolocation.getCurrentPosition(
-        handleGeolocationSuccess,
-        handleGeolocationError
+      handleGeolocationSuccess,
+      handleGeolocationError
     );
   }, [userPosition, zoom]);
 
@@ -62,9 +95,9 @@ const MapComponent = ({ zoom, destinationMarker, initialLocation }) => {
           <Popup>Your Marker Popup</Popup>
         </Marker>
         {destinationMarker && (
-            <Marker position={destinationMarker} icon={customMarkerIcon}>
-              <Popup>Your Destination Marker Popup</Popup>
-            </Marker>
+          <Marker position={destinationMarker} icon={customMarkerIcon}>
+            <Popup>Your Destination Marker Popup</Popup>
+          </Marker>
         )}
       </MapContainer>
     </div>
--- SmartMap/vite.config.js
+++ SmartMap/vite.config.js
@@ -1,6 +1,8 @@
 
 import path from 'path';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 
 
 const SRC_DIR = path.resolve(__dirname, './src');
@@ -11,7 +13,7 @@ export default async () => {
   return  {
     plugins: [
       react(),
-
+      mkcert(),
     ],
     root: SRC_DIR,
     base: '',
@@ -31,7 +33,7 @@ export default async () => {
     },
     server: {
       host: true,
+      https: true,
     },
-
   };
 }
